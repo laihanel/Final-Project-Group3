99a5f32be447cf12c8c8dc1dc0d12bce89ba236a
herrzilinski <herrzilinski@gmail.com>
2022-04-14 17:31:33 -0400

    Model_CNN3D Update

--- Code/Model_CNN3D.py
+++ Code/Model_CNN3D.py
@@ -1,19 +1,18 @@
+import timeit
+from tqdm import tqdm
 import torch
 import torch.nn as nn
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-from UCF101_9_Load_Data import read_data
+from torchvision import models
+from UCF101_9_Load_Data import train_ds, test_ds
 
 # %% HyperParameters
 NICKNAME = 'Trial_9class'
 OUTPUTS_a = 9  # Subject to change, now we manually picked 9 classes to classify
 LR = 0.001
-n_epoch = 10
+n_epoch = 3
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-PRETRAINED = False
-# PRETRAINED = models.efficientnet_b4(pretrained=True)
-
-# %%
-train_ds, test_ds = read_data()
+PRETRAINED = models.efficientnet_b4(pretrained=True)
 
 # %% Create the model
 class CNN(nn.Module):
@@ -77,12 +76,63 @@ def save_model(model):
 model, optimizer, criterion, scheduler = model_definition(PRETRAINED)
 print(model)
 # Fit data to model
+# for epoch in range(n_epoch):
+#     for xdata, xtarget in train_ds:
+#         xdata, xtarget = xdata.to(device), xtarget.to(device)
+#         optimizer.zero_grad()
+#         output = model(xdata)
+#         loss = criterion(output, xtarget)
+#         loss.backward()
+#         optimizer.step()
+#         print(loss)
+
 for epoch in range(n_epoch):
-    for xdata, xtarget in train_ds:
-        xdata, xtarget = xdata.to(device), xtarget.to(device)
-        optimizer.zero_grad()
-        output = model(xdata)
-        loss = criterion(output, xtarget)
-        loss.backward()
-        optimizer.step()
-        print(loss)
+    # each epoch has a training and validation step
+    for phase in ['train', 'val']:
+        start_time = timeit.default_timer()
+
+        # reset the running loss and corrects
+        running_loss = 0.0
+        running_corrects = 0.0
+
+        # set model to train() or eval() mode depending on whether it is trained
+        # or being validated. Primarily affects layers such as BatchNorm or Dropout.
+        if phase == 'train':
+            # scheduler.step() is to be called once every epoch during training
+            # optimizer.step()
+            scheduler.step()
+            model.train()
+        else:
+            model.eval()
+
+        for inputs, labels in tqdm(trainval_loaders[phase]):
+            # move inputs and labels to the device the training is taking place on
+            inputs = Variable(inputs, requires_grad=True).to(device)
+            labels = Variable(labels).to(device)
+            optimizer.zero_grad()
+
+            if phase == 'train':
+                outputs = model(inputs)
+            else:
+                with torch.no_grad():
+                    outputs = model(inputs)
+
+            probs = nn.Softmax(dim=1)(outputs)
+            preds = torch.max(probs, 1)[1]
+            loss = criterion(outputs, labels)
+            #             loss = criterion(preds, labels)
+
+            if phase == 'train':
+                loss.backward()
+                optimizer.step()
+
+            running_loss += loss.item() * inputs.size(0)
+            running_corrects += torch.sum(preds == labels.data)
+        #             print(f'running loss: {running_loss},\n inputs: {inputs} \n output: {outputs} {len(outputs)} \n labels: {labels} {len(labels)}')
+
+        epoch_loss = running_loss / trainval_sizes[phase]
+        epoch_acc = running_corrects.double() / trainval_sizes[phase]
+
+        print("[{}] Epoch: {}/{} Loss: {} Acc: {}".format(phase, epoch + 1, nEpochs, epoch_loss, epoch_acc))
+        stop_time = timeit.default_timer()
+        print("Execution time: " + str(stop_time - start_time) + "\n")
