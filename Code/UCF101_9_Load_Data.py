0de43e248ee13ccf0103abb136554ffb2ed1650a
Aihan Liu <laihanel@hotmail.com>
2022-04-14 15:33:31 -0400

    Add an initial CNN, not sure if it works

--- Code/UCF101_9_Load_Data.py
+++ Code/UCF101_9_Load_Data.py
@@ -45,20 +45,71 @@ def custom_collate(batch):
 
 
 # %%
-# create train loader (allowing batches and other extras)
-train_dataset = UCF101(DATA_DIR, LABEL_DIR, frames_per_clip=frames_per_clip,
-                       step_between_clips=step_between_clips, train=True, transform=tfs)
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
-                                           collate_fn=custom_collate)
-# create test loader (allowing batches and other extras)
-test_dataset = UCF101(DATA_DIR, LABEL_DIR, frames_per_clip=frames_per_clip,
-                      step_between_clips=step_between_clips, train=False, transform=tfs)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True,
-                                          collate_fn=custom_collate)
+def read_data():
+    # create train loader (allowing batches and other extras)
+    train_dataset = UCF101(DATA_DIR, LABEL_DIR, frames_per_clip=frames_per_clip,
+                           step_between_clips=step_between_clips, train=True, transform=tfs)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
+                                               collate_fn=custom_collate)
+    # create test loader (allowing batches and other extras)
+    test_dataset = UCF101(DATA_DIR, LABEL_DIR, frames_per_clip=frames_per_clip,
+                          step_between_clips=step_between_clips, train=False, transform=tfs)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True,
+                                              collate_fn=custom_collate)
 
-# %%
-print(f"Total number of train samples: {len(train_dataset)}")
-print(f"Total number of test samples: {len(test_dataset)}")
-print(f"Total number of (train) batches: {len(train_loader)}")
-print(f"Total number of (test) batches: {len(test_loader)}")
-print()
+    # %%
+    print(f"Total number of train samples: {len(train_dataset)}")
+    print(f"Total number of test samples: {len(test_dataset)}")
+    print(f"Total number of (train) batches: {len(train_loader)}")
+    print(f"Total number of (test) batches: {len(test_loader)}")
+    print()
+    return train_loader, test_loader
+
+
+train_ds, test_ds = read_data()
+
+# Create the model
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv3D(3, 32, kernel_size=(3, 3, 2))
+        self.convnorm1 = nn.BatchNorm3d(32)
+        self.pool1 = nn.MaxPool3d((2, 2, 2))
+        self.drop1 = nn.Dropout(0.5)
+        self.conv2 = nn.Conv3D(32, 64, kernel_size=(3, 3, 2))
+        self.pool2 = nn.MaxPool3d((2, 2, 2))
+        self.convnorm2 = nn.BatchNorm3d(64)
+        self.drop2 = nn.Dropout(0.5)
+
+        self.global_avg_pool = nn.AvgPool3d((1, 1, 2))
+        self.linear1 = nn.Linear(64, 128)
+        self.linear2 = nn.Linear(128, 9)
+        self.act = torch.relu
+
+
+    def forward(self, x):
+        x = self.drop1(self.pool1(self.convnorm1(self.act(self.conv1(x)))))
+        x = self.drop2(self.pool2(self.convnorm2(self.act(self.conv2(x)))))
+        # x = self.pool3(self.dropout3(self.act(self.conv4(self.act(self.conv3(x))))))
+        return self.linear2(self.linear1(self.global_avg_pool(x).view(-1, -1, 64)))
+
+LR = 0.001
+n_epoch = 10
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+model = CNN()
+model = model.to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=LR)
+criterion = nn.CrossEntropyLoss()
+# Compile the model
+
+model.summary()
+# Fit data to model
+for epoch in range(n_epoch):
+    for xdata, xtarget in train_ds:
+        xdata, xtarget = xdata.to(device), xtarget.to(device)
+        optimizer.zero_grad()
+        output = model(xdata)
+        loss = criterion(output, xtarget)
+        loss.backward()
+        optimizer.step()
+        print(loss)
